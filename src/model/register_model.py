728680f50e32c62673484c31237a53c8b11a6c94
ashish shukla <ashishshukla888.ak@com>
2024-08-06 14:00:21 +0530

    added tracking uri

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -21,6 +21,8 @@ dagshub_url = "https://dagshub.com"
 repo_owner = "ashishshukla888"
 repo_name = "mlops-mini-proj"
 
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # logging configuration
 logger = logging.getLogger('model_registration')
